3d37066f71da33d3451ebfa838a00d6204758d6c
Tobias Bosch <tbosch1009@gmail.com>
2013-12-16 10:05:44 -0800

    v1.2.5

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.6-build.1989+sha.b0474cb
+ * @license AngularJS v1.2.5
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.1989+sha.b0474cb/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.5/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1829,11 +1829,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.6-build.1989+sha.b0474cb',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.5',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
-  dot: 6,
-  codeName: 'taco-salsafication'
+  dot: 5,
+  codeName: 'singularity-expansion'
 };
 
 
