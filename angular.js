7a8f121cc0676ce0bfb821865476ac6bbff42373
jenkins <ci-bot@angularjs.org>
2013-12-17 04:42:12 -0800

    v1.2.6-build.1996+sha.6ac773f

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.6-build.1994+sha.4f72433
+ * @license AngularJS v1.2.6-build.1996+sha.6ac773f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.1994+sha.4f72433/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.1996+sha.6ac773f/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1829,7 +1829,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.6-build.1994+sha.4f72433',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.6-build.1996+sha.6ac773f',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 6,
