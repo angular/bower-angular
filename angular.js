b3b6094e41f19f5c847fb53bd3b5a0590bbc99bf
jenkins <ci-bot@angularjs.org>
2013-12-19 16:17:58 -0800

    v1.2.7-build.2020+sha.af7203e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.6
+ * @license AngularJS v1.2.7-build.2020+sha.af7203e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.6/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2020+sha.af7203e/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1831,10 +1831,10 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.6',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.7-build.2020+sha.af7203e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
-  dot: 6,
+  dot: 7,
   codeName: 'taco-salsafication'
 };
 
