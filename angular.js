5f2117108e42b004095786fbbc3c524795d3dbca
jenkins <ci-bot@angularjs.org>
2013-12-23 18:53:05 -0800

    v1.2.7-build.2026+sha.7d6e5a2

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2025+sha.d1c4766
+ * @license AngularJS v1.2.7-build.2026+sha.7d6e5a2
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2025+sha.d1c4766/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2026+sha.7d6e5a2/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1831,7 +1831,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.7-build.2025+sha.d1c4766',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.7-build.2026+sha.7d6e5a2',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 7,
