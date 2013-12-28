92ffa8d4195afa3dbfd7338c2fe7892aa096c604
jenkins <ci-bot@angularjs.org>
2013-12-27 23:08:49 -0800

    v1.2.7-build.2028+sha.498365f

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2027+sha.056c849
+ * @license AngularJS v1.2.7-build.2028+sha.498365f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2027+sha.056c849/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2028+sha.498365f/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1831,7 +1831,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.7-build.2027+sha.056c849',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.7-build.2028+sha.498365f',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 7,
