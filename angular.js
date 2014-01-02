b64ab35a8b36fafacb2cf7d5bb79ef800f30948f
jenkins <ci-bot@angularjs.org>
2014-01-02 15:58:36 -0800

    v1.2.7-build.2040+sha.884ef0d

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2039+sha.4f57236
+ * @license AngularJS v1.2.7-build.2040+sha.884ef0d
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2039+sha.4f57236/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2040+sha.884ef0d/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1831,7 +1831,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.7-build.2039+sha.4f57236',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.7-build.2040+sha.884ef0d',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 7,
@@ -11531,6 +11531,7 @@ function $RootScopeProvider(){
 
         return function() {
           arrayRemove(array, watcher);
+          lastDirtyWatch = null;
         };
       },
 
