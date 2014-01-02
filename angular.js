d31b2c1e7380890324df249fa8a1f9d4cc00b6e7
jenkins <ci-bot@angularjs.org>
2014-01-02 15:28:59 -0800

    v1.2.7-build.2039+sha.4f57236

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2038+sha.50bf029
+ * @license AngularJS v1.2.7-build.2039+sha.4f57236
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2038+sha.50bf029/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.7-build.2039+sha.4f57236/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1831,7 +1831,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.7-build.2038+sha.50bf029',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.7-build.2039+sha.4f57236',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 7,
@@ -7941,6 +7941,11 @@ function createHttpBackend($browser, XHR, $browserDefer, callbacks, rawDocument)
       // always async
       xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
+          // onreadystatechange might by called multiple times
+          // with readyState === 4 on mobile webkit caused by
+          // xhrs that are resolved while the app is in the background (see #5426).
+          xhr.onreadystatechange = undefined;
+
           var responseHeaders = null,
               response = null;
 
