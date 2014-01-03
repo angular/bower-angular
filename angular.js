47b3a318f54e404133691f310bc6da6db621116d
jenkins <ci-bot@angularjs.org>
2014-01-03 12:15:04 -0800

    v1.2.8-build.2061+sha.86151b0

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.8-build.2060+sha.1b7a6c6
+ * @license AngularJS v1.2.8-build.2061+sha.86151b0
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2060+sha.1b7a6c6/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2061+sha.86151b0/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1833,11 +1833,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.8-build.2060+sha.1b7a6c6',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.8-build.2061+sha.86151b0',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 8,
-  codeName: 'emoji-clairvoyance'
+  codeName: 'interdimensional-cartography '
 };
 
 
