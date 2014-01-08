24c0a22b32631277c6da4c419a484278e560c7a1
jenkins <ci-bot@angularjs.org>
2014-01-08 00:49:35 -0800

    v1.2.8-build.2092+sha.95e1b2d

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.8-build.2091+sha.75345e3
+ * @license AngularJS v1.2.8-build.2092+sha.95e1b2d
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2091+sha.75345e3/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2092+sha.95e1b2d/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1833,7 +1833,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.8-build.2091+sha.75345e3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.8-build.2092+sha.95e1b2d',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 8,
@@ -7937,7 +7937,7 @@ function createHttpBackend($browser, createXhr, $browserDefer, callbacks, rawDoc
         } else {
           completeRequest(callback, status || -2);
         }
-        delete callbacks[callbackId];
+        callbacks[callbackId] = angular.noop;
       });
     } else {
 
