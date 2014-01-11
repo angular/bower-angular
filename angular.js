d78e1b49b89b52a9b9c4c5febac75b387e63b5b3
jenkins <ci-bot@angularjs.org>
2014-01-10 20:29:11 -0800

    v1.2.9-build.2116+sha.cde840f

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.9-build.2115+sha.63150d8
+ * @license AngularJS v1.2.9-build.2116+sha.cde840f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.9-build.2115+sha.63150d8/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.9-build.2116+sha.cde840f/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1833,7 +1833,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.9-build.2115+sha.63150d8',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.9-build.2116+sha.cde840f',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 9,
