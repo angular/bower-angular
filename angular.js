19e78e10df825c2e08d7fdee2284fe4c80daa65b
jenkins <ci-bot@angularjs.org>
2014-01-23 13:59:44 -0800

    v1.2.10-build.2178+sha.40dc806

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.10-build.2177+sha.5e9835b
+ * @license AngularJS v1.2.10-build.2178+sha.40dc806
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2177+sha.5e9835b/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.10-build.2178+sha.40dc806/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1834,7 +1834,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.10-build.2177+sha.5e9835b',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.10-build.2178+sha.40dc806',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 10,
