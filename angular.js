58c63b3d0a111117ec31d7157f69314504bd1c11
jenkins <ci-bot@angularjs.org>
2014-02-03 09:58:13 -0800

    v1.2.12-build.2206+sha.106af49

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.11
+ * @license AngularJS v1.2.12-build.2206+sha.106af49
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.11/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.12-build.2206+sha.106af49/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1834,11 +1834,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.11',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.12-build.2206+sha.106af49',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
-  dot: 11,
-  codeName: 'cryptocurrency-hyperdeflation'
+  dot: 12,
+  codeName: 'cauliflower-eradication'
 };
 
 
