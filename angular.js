9cadac0960fc5a3078821f2e6939c6e2fe6d603d
jenkins <ci-bot@angularjs.org>
2014-02-06 17:28:59 -0800

    v1.2.12-build.2233+sha.16301be

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.12-build.2232+sha.95be253
+ * @license AngularJS v1.2.12-build.2233+sha.16301be
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.12-build.2232+sha.95be253/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.12-build.2233+sha.16301be/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1834,7 +1834,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.12-build.2232+sha.95be253',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.12-build.2233+sha.16301be',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 12,
@@ -14959,7 +14959,14 @@ var htmlAnchorDirective = valueFn({
 
           element(by.id('link-3')).click();
 
-          expect(browser.driver.getCurrentUrl()).toMatch(/\/123$/);
+          // At this point, we navigate away from an Angular page, so we need
+          // to use browser.driver to get the base webdriver.
+
+          browser.wait(function() {
+            return browser.driver.getCurrentUrl().then(function(url) {
+              return url.match(/\/123$/);
+            });
+          }, 1000, 'page should navigate to /123');
         });
 
         it('should execute ng-click but not reload when href empty string and name specified', function() {
