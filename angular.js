6f1130f29552515dfa090c087bac497527638e65
jenkins <ci-bot@angularjs.org>
2014-02-07 20:56:20 -0800

    v1.2.13-build.2242+sha.e645f7c

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.13-build.2241+sha.600e621
+ * @license AngularJS v1.2.13-build.2242+sha.e645f7c
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2241+sha.600e621/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2242+sha.e645f7c/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1834,7 +1834,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.13-build.2241+sha.600e621',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.13-build.2242+sha.e645f7c',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 13,
