0fbf0fdf3a805513a5f20da6b6060fa589972be5
jenkins <ci-bot@angularjs.org>
2014-02-10 17:04:01 -0800

    v1.2.13-build.2247+sha.a686244

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.13-build.2246+sha.945fc1a
+ * @license AngularJS v1.2.13-build.2247+sha.a686244
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2246+sha.945fc1a/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2247+sha.a686244/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1834,7 +1834,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.13-build.2246+sha.945fc1a',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.13-build.2247+sha.a686244',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 13,
@@ -18256,8 +18256,8 @@ forEach(
  * Enables binding angular expressions to onsubmit events.
  *
  * Additionally it prevents the default action (which for form means sending the request to the
- * server and reloading the current page) **but only if the form does not contain an `action`
- * attribute**.
+ * server and reloading the current page), but only if the form does not contain `action`,
+ * `data-action`, or `x-action` attributes.
  *
  * @element form
  * @priority 0
