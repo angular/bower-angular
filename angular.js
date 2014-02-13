35e67903f8fe8b97dc7e68633d6de9a021285208
jenkins <ci-bot@angularjs.org>
2014-02-13 13:58:33 -0800

    v1.2.13-build.2262+sha.a9fcb0d

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.13-build.2261+sha.a3846ab
+ * @license AngularJS v1.2.13-build.2262+sha.a9fcb0d
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2261+sha.a3846ab/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2262+sha.a9fcb0d/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1836,7 +1836,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.13-build.2261+sha.a3846ab',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.13-build.2262+sha.a9fcb0d',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 13,
@@ -16109,7 +16109,8 @@ var inputType = {
   'hidden': noop,
   'button': noop,
   'submit': noop,
-  'reset': noop
+  'reset': noop,
+  'file': noop
 };
 
 // A helper function to call $setValidity and return the value / undefined,
