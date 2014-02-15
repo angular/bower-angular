503f73926c7434570112dcae3fb23c12392d6547
jenkins <ci-bot@angularjs.org>
2014-02-15 11:09:00 -0800

    v1.2.14-build.2267+sha.cceb455

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2266+sha.3018ff7
+ * @license AngularJS v1.2.14-build.2267+sha.cceb455
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2266+sha.3018ff7/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2267+sha.cceb455/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1836,7 +1836,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.14-build.2266+sha.3018ff7',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.14-build.2267+sha.cceb455',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 14,
@@ -14335,7 +14335,7 @@ function numberFilter($locale) {
 
 var DECIMAL_SEP = '.';
 function formatNumber(number, pattern, groupSep, decimalSep, fractionSize) {
-  if (isNaN(number) || !isFinite(number)) return '';
+  if (number == null || !isFinite(number) || isObject(number)) return '';
 
   var isNegative = number < 0;
   number = Math.abs(number);
