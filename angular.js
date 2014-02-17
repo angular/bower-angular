70166b71e952ae030d7d4f33f587e004cffa8897
jenkins <ci-bot@angularjs.org>
2014-02-17 06:23:46 -0800

    v1.2.14-build.2271+sha.e23a6a3

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2270+sha.47ec6f5
+ * @license AngularJS v1.2.14-build.2271+sha.e23a6a3
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2270+sha.47ec6f5/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2271+sha.e23a6a3/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1878,7 +1878,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.14-build.2270+sha.47ec6f5',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.14-build.2271+sha.e23a6a3',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 14,
@@ -4096,9 +4096,8 @@ var $AnimateProvider = ['$provide', function($provide) {
 
       /**
        *
-       * @ngdoc function
-       * @name ng.$animate#setClass
-       * @methodOf ng.$animate
+       * @ngdoc method
+       * @name $animate#setClass
        * @function
        * @description Adds and/or removes the given CSS classes to and from the element.
        * Once complete, the done() callback will be fired (if provided).
