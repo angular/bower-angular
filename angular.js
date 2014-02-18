56e0422be801e8ae00104ca0572698b5391f3718
jenkins <ci-bot@angularjs.org>
2014-02-17 22:57:14 -0800

    v1.2.14-build.2275+sha.4f937bd

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2273+sha.666137d
+ * @license AngularJS v1.2.14-build.2275+sha.4f937bd
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2273+sha.666137d/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.14-build.2275+sha.4f937bd/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1878,7 +1878,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.14-build.2273+sha.666137d',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.14-build.2275+sha.4f937bd',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 14,
@@ -8367,7 +8367,7 @@ function $InterpolateProvider() {
      * Use {@link ng.$interpolateProvider#methods_endSymbol $interpolateProvider#endSymbol} to change
      * the symbol.
      *
-     * @returns {string} start symbol.
+     * @returns {string} end symbol.
      */
     $interpolate.endSymbol = function() {
       return endSymbol;
