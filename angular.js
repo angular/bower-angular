09c34b160f12cf83de1f83afd4fa5df07318fa01
jenkins <ci-bot@angularjs.org>
2014-03-02 22:38:29 -0800

    v1.2.15-build.2364+sha.7c34e1f

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.15-build.2363+sha.bf82c8a
+ * @license AngularJS v1.2.15-build.2364+sha.7c34e1f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2363+sha.bf82c8a/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2364+sha.7c34e1f/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1880,7 +1880,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.15-build.2363+sha.bf82c8a',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.15-build.2364+sha.7c34e1f',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 15,
