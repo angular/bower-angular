a7cb4a40fec1f5f620e7f3b6423935752187e404
jenkins <ci-bot@angularjs.org>
2014-03-07 16:30:00 -0800

    v1.3.0-beta.1

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.15-build.2399+sha.ca4ddfa
+ * @license AngularJS v1.3.0-beta.1
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2399+sha.ca4ddfa/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-beta.1/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1878,11 +1878,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.15-build.2399+sha.ca4ddfa',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-beta.1',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
-  minor: 2,
-  dot: 15,
-  codeName: 'snapshot'
+  minor: 3,
+  dot: 0,
+  codeName: 'retractable-eyebrow'
 };
 
 
