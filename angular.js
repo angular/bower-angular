44ffbc311a07432b5e897801377ba237fdf5cf18
jenkins <ci-bot@angularjs.org>
2014-03-14 04:18:20 -0700

    v1.3.0-build.2425+sha.3cc02e7

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2424+sha.79592ce
+ * @license AngularJS v1.3.0-build.2425+sha.3cc02e7
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2424+sha.79592ce/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2425+sha.3cc02e7/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1878,7 +1878,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2424+sha.79592ce',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2425+sha.3cc02e7',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: undefined,
