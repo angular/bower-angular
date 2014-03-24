f06d8dd53a1f87ff290b69fb452f1e3ee4211db6
jenkins <ci-bot@angularjs.org>
2014-03-24 08:01:27 -0700

    v1.3.0-build.2494+sha.b472d02

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2493+sha.8339c2e
+ * @license AngularJS v1.3.0-build.2494+sha.b472d02
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2493+sha.8339c2e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2494+sha.b472d02/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1919,7 +1919,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2493+sha.8339c2e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2494+sha.b472d02',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -16808,7 +16808,6 @@ function addNativeHtml5Validators(ctrl, validatorName, element) {
       return value;
     };
     ctrl.$parsers.push(validator);
-    ctrl.$formatters.push(validator);
   }
 }
 
