99d07453c4542909432e2e0bce33dc94e60fb3f2
jenkins <ci-bot@angularjs.org>
2014-03-28 11:11:27 -0700

    v1.2.16-build.60+sha.8a59724

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.16-build.59+sha.35d635c
+ * @license AngularJS v1.2.16-build.60+sha.8a59724
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.16-build.59+sha.35d635c/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.16-build.60+sha.8a59724/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1919,7 +1919,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.16-build.59+sha.35d635c',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.16-build.60+sha.8a59724',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 16,
