4b0019abb01caf8b0b48af295bdab8bc953ed9a3
jenkins <ci-bot@angularjs.org>
2014-05-16 18:26:57 -0700

    v1.3.0-build.2710+sha.96fa705

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-beta.9
+ * @license AngularJS v1.3.0-build.2710+sha.96fa705
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-beta.9/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2710+sha.96fa705/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2037,11 +2037,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-beta.9',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2710+sha.96fa705',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
-  codeName: 'release-naming'
+  codeName: 'snapshot'
 };
 
 
