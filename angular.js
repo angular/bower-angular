3b49ecb8aa14076ac8e95ff12e6c487bf63f8fff
jenkins <ci-bot@angularjs.org>
2014-05-24 15:07:36 -0700

    v1.3.0-build.2767+sha.3fc95e0

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-beta.10
+ * @license AngularJS v1.3.0-build.2767+sha.3fc95e0
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-beta.10/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2767+sha.3fc95e0/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2037,11 +2037,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-beta.10',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2767+sha.3fc95e0',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
-  codeName: 'excessive-clarification'
+  codeName: 'snapshot'
 };
 
 
