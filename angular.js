67335e3329c93b6a77a6a49966701b43f457bcc8
jenkins <ci-bot@angularjs.org>
2014-06-06 12:36:29 -0700

    v1.2.17-build.232+sha.6acc73f

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.231+sha.a4367ab
+ * @license AngularJS v1.2.17-build.232+sha.6acc73f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.231+sha.a4367ab/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.17-build.232+sha.6acc73f/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1949,7 +1949,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.17-build.231+sha.a4367ab',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.17-build.232+sha.6acc73f',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 17,
