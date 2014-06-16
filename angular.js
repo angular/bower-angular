55ec47906ec3e7024d664f96b78af7b9ba6a4987
jenkins <ci-bot@angularjs.org>
2014-06-16 13:54:49 -0700

    v1.3.0-build.2837+sha.928c7ec

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2836+sha.d50829b
+ * @license AngularJS v1.3.0-build.2837+sha.928c7ec
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2836+sha.d50829b/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2837+sha.928c7ec/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2071,7 +2071,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2836+sha.d50829b',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2837+sha.928c7ec',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -13866,7 +13866,7 @@ function $SceProvider() {
 
     /**
      * @ngdoc method
-     * @name $sce#parse
+     * @name $sce#parseAs
      *
      * @description
      * Converts Angular {@link guide/expression expression} into a function.  This is like {@link
