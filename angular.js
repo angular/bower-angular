194cbe6fdb2fad14d6918d0300e824bef00aeca8
jenkins <ci-bot@angularjs.org>
2014-06-20 09:09:59 -0700

    v1.3.0-build.2845+sha.e57ad6a

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2844+sha.dafb8a3
+ * @license AngularJS v1.3.0-build.2845+sha.e57ad6a
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2844+sha.dafb8a3/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2845+sha.e57ad6a/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2073,7 +2073,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2844+sha.dafb8a3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2845+sha.e57ad6a',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -3555,7 +3555,7 @@ function annotate(fn, strictDi, name) {
 
 
 /**
- * @ngdoc object
+ * @ngdoc service
  * @name $provide
  *
  * @description
