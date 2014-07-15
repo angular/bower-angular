a113196598ef8c41d1aaf2180bc1c215081c4a94
jenkins <ci-bot@angularjs.org>
2014-07-15 05:52:51 -0700

    v1.3.0-build.2927+sha.92bceb5

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2926+sha.528f56a
+ * @license AngularJS v1.3.0-build.2927+sha.92bceb5
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2926+sha.528f56a/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2927+sha.92bceb5/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2080,7 +2080,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2926+sha.528f56a',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2927+sha.92bceb5',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -15733,6 +15733,10 @@ function orderByFilter($parse){
       var t1 = typeof v1;
       var t2 = typeof v2;
       if (t1 == t2) {
+        if (isDate(v1) && isDate(v2)) {
+          v1 = v1.valueOf();
+          v2 = v2.valueOf();
+        }
         if (t1 == "string") {
            v1 = v1.toLowerCase();
            v2 = v2.toLowerCase();
