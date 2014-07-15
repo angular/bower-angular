39ce267f0d73d8c8a72878fb91551aa8016c232a
jenkins <ci-bot@angularjs.org>
2014-07-15 06:48:49 -0700

    v1.2.21-build.308+sha.fe01a85

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.21-build.307+sha.fe01a85
+ * @license AngularJS v1.2.21-build.308+sha.fe01a85
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.21-build.307+sha.fe01a85/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.21-build.308+sha.fe01a85/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1972,7 +1972,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.21-build.307+sha.fe01a85',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.21-build.308+sha.fe01a85',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 21,
