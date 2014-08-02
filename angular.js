52855e89e79dd973a79fd1983d8cf55f2075771a
jenkins <ci-bot@angularjs.org>
2014-08-01 19:47:19 -0700

    v1.2.22-build.361+sha.bcfa64e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.22-build.360+sha.bcfa64e
+ * @license AngularJS v1.2.22-build.361+sha.bcfa64e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.22-build.360+sha.bcfa64e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.22-build.361+sha.bcfa64e/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1977,7 +1977,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.22-build.360+sha.bcfa64e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.22-build.361+sha.bcfa64e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 22,
