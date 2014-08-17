e8139b3749fc07aa3f6ba6f36d4c5d78d1af4c1a
jenkins <ci-bot@angularjs.org>
2014-08-17 15:01:54 -0700

    v1.3.0-build.3055+sha.187b1b8

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3054+sha.b24e381
+ * @license AngularJS v1.3.0-build.3055+sha.187b1b8
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3054+sha.b24e381/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3055+sha.187b1b8/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2084,7 +2084,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3054+sha.b24e381',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3055+sha.187b1b8',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -2948,7 +2948,7 @@ forEach({
     }
 
     // http://jsperf.com/string-indexof-vs-split
-    var types = type.indexOf(' ') ? type.split(' ') : [type];
+    var types = type.indexOf(' ') >= 0 ? type.split(' ') : [type];
     var i = types.length;
 
     while (i--) {
