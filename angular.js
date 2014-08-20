4c415b835f1e364bbc43c248b33e012e2b222408
jenkins <ci-bot@angularjs.org>
2014-08-19 17:38:53 -0700

    v1.3.0-build.3072+sha.f02f7d9

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3071+sha.7eae29e
+ * @license AngularJS v1.3.0-build.3072+sha.f02f7d9
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3071+sha.7eae29e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3072+sha.f02f7d9/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2078,7 +2078,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3071+sha.7eae29e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3072+sha.f02f7d9',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -7268,6 +7268,13 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
               }
             }
             $rootElement.length -= removeCount - 1;
+
+            // If the replaced element is also the jQuery .context then replace it
+            // .context is a deprecated jQuery api, so we should set it only when jQuery set it
+            // http://api.jquery.com/context/
+            if ($rootElement.context === firstElementToRemove) {
+              $rootElement.context = newNode;
+            }
             break;
           }
         }
