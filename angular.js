7fdf47b948d09d126fdfd1e9a4fcf5bdbff54b9f
jenkins <ci-bot@angularjs.org>
2014-09-04 04:53:27 -0700

    v1.3.0-build.3197+sha.6d1e7cd

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3196+sha.86c6be8
+ * @license AngularJS v1.3.0-build.3197+sha.6d1e7cd
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3196+sha.86c6be8/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3197+sha.6d1e7cd/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2114,7 +2114,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3196+sha.86c6be8',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3197+sha.6d1e7cd',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -6256,6 +6256,10 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
         var $linkNode = cloneConnectFn
           ? JQLitePrototype.clone.call(namespaceAdaptedCompileNodes) // IMPORTANT!!!
           : namespaceAdaptedCompileNodes;
+
+        if ( $linkNode.length === 0 && parentBoundTranscludeFn ) {
+          $linkNode = parentBoundTranscludeFn(scope);
+        }
 
         if (transcludeControllers) {
           for (var controllerName in transcludeControllers) {
