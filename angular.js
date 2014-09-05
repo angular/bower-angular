be3632de98c57fe33f0f4acdd8b1a93e20ee3a36
jenkins <ci-bot@angularjs.org>
2014-09-05 11:22:20 -0700

    v1.3.0-build.3209+sha.b3b6721

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3208+sha.8b5d33d
+ * @license AngularJS v1.3.0-build.3209+sha.b3b6721
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3208+sha.8b5d33d/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3209+sha.b3b6721/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2114,7 +2114,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3208+sha.8b5d33d',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3209+sha.b3b6721',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -2152,7 +2152,8 @@ function publishExternalAPI(angular){
     'getTestability': getTestability,
     '$$minErr': minErr,
     '$$csp': csp,
-    'reloadWithDebugInfo': reloadWithDebugInfo
+    'reloadWithDebugInfo': reloadWithDebugInfo,
+    '$$hasClass': jqLiteHasClass
   });
 
   angularModule = setupModuleLoader(window);
