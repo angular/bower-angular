e2acb3e79e8f571bd36e12f31c7b2587e0c9dff3
jenkins <ci-bot@angularjs.org>
2014-09-09 16:32:00 -0700

    v1.2.24

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.24-build.444+sha.66503a6
+ * @license AngularJS v1.2.24
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.24-build.444+sha.66503a6/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.24/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1987,11 +1987,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.24-build.444+sha.66503a6',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.24',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 24,
-  codeName: 'snapshot'
+  codeName: 'static-levitation'
 };
 
 
