c7bdad874c48aa3cf83a8baca35a03eeba7a2cf7
jenkins <ci-bot@angularjs.org>
2014-09-09 16:03:21 -0700

    v1.3.0-rc.1

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3229+sha.3c538c1
+ * @license AngularJS v1.3.0-rc.1
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3229+sha.3c538c1/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-rc.1/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2122,11 +2122,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3229+sha.3c538c1',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-rc.1',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
-  codeName: 'snapshot'
+  codeName: 'backyard-atomicity'
 };
 
 
