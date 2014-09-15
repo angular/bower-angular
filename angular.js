0fe074f52a4de07a03bb6601914002677d38bdb1
jenkins <ci-bot@angularjs.org>
2014-09-15 12:39:43 -0700

    v1.3.0-build.3256+sha.cfdd161

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3255+sha.06280a1
+ * @license AngularJS v1.3.0-build.3256+sha.cfdd161
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3255+sha.06280a1/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3256+sha.cfdd161/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2122,7 +2122,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3255+sha.06280a1',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3256+sha.cfdd161',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -20297,6 +20297,8 @@ var ngValueDirective = function() {
  * Any pending changes will take place immediately when an enclosing form is submitted via the
  * `submit` event. Note that `ngClick` events will occur before the model is updated. Use `ngSubmit`
  * to have access to the updated model.
+ *
+ * `ngModelOptions` has an effect on the element it's declared on and its descendants.
  *
  * @param {Object} ngModelOptions options to apply to the current model. Valid keys are:
  *   - `updateOn`: string specifying which event should be the input bound to. You can set several
