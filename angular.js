361c104e08d9dd660204e0a4f24d1218f841f628
jenkins <ci-bot@angularjs.org>
2014-10-07 10:59:32 -0700

    v1.3.0-build.3360+sha.ab24019

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3359+sha.613d0a3
+ * @license AngularJS v1.3.0-build.3360+sha.ab24019
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3359+sha.613d0a3/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3360+sha.ab24019/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2114,7 +2114,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3359+sha.613d0a3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3360+sha.ab24019',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -3447,7 +3447,7 @@ function annotate(fn, strictDi, name) {
  *   expect($injector.get('$injector')).toBe($injector);
  *   expect($injector.invoke(function($injector) {
  *     return $injector;
- *   }).toBe($injector);
+ *   })).toBe($injector);
  * ```
  *
  * # Injection Function Annotation
