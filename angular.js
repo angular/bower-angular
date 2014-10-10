740d38da45ee706fd719d407fc0152158e8e856a
jenkins <ci-bot@angularjs.org>
2014-10-10 13:31:25 -0700

    v1.3.0-build.3407+sha.9ba24c5

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3406+sha.4474633
+ * @license AngularJS v1.3.0-build.3407+sha.9ba24c5
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3406+sha.4474633/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3407+sha.9ba24c5/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2122,7 +2122,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3406+sha.4474633',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3407+sha.9ba24c5',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -8990,7 +8990,7 @@ function $HttpProvider() {
       function transformResponse(response) {
         // make a copy since the response must be cacheable
         var resp = extend({}, response);
-        if(config.method === 'HEAD'){
+        if (!response.data) {
           resp.data = response.data;
         } else {
           resp.data = transformData(response.data, response.headers, config.transformResponse);
