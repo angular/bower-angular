8ee0a57f5c389dfe3d0e92e58732404dcdc5fb1f
jenkins <ci-bot@angularjs.org>
2014-10-15 09:34:18 -0700

    v1.3.1-build.3431+sha.ed85ec4

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.1-build.3430+sha.6502ab0
+ * @license AngularJS v1.3.1-build.3431+sha.ed85ec4
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.1-build.3430+sha.6502ab0/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.1-build.3431+sha.ed85ec4/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2136,7 +2136,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.1-build.3430+sha.6502ab0',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.1-build.3431+sha.ed85ec4',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 1,
@@ -20583,6 +20583,7 @@ var NgModelController = ['$scope', '$exceptionHandler', '$attrs', '$element', '$
  * @name ngModel
  *
  * @element input
+ * @priority 1
  *
  * @description
  * The `ngModel` directive binds an `input`,`select`, `textarea` (or custom form control) to a
