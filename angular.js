06dcb4fea5442fca29c12003b3de443cf7ea70b0
jenkins <ci-bot@angularjs.org>
2014-10-16 04:08:03 -0700

    v1.3.1-build.3434+sha.22407a9

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.1-build.3433+sha.fc70a98
+ * @license AngularJS v1.3.1-build.3434+sha.22407a9
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.1-build.3433+sha.fc70a98/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.1-build.3434+sha.22407a9/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2136,7 +2136,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.1-build.3433+sha.fc70a98',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.1-build.3434+sha.22407a9',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 1,
@@ -2765,7 +2765,6 @@ var JQLitePrototype = JQLite.prototype = {
       // jshint -W064
       JQLite(window).on('load', trigger); // fallback to window.onload for others
       // jshint +W064
-      this.on('DOMContentLoaded', trigger);
     }
   },
   toString: function() {
