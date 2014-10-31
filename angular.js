e7de98173a34859451076bf528d435265fe36504
jenkins <ci-bot@angularjs.org>
2014-10-31 10:47:38 -0700

    v1.3.1

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.1-build.3507+sha.ed6e91b
+ * @license AngularJS v1.3.1
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.1-build.3507+sha.ed6e91b/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.1/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2089,11 +2089,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.1-build.3507+sha.ed6e91b',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.1',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 1,
-  codeName: 'snapshot'
+  codeName: 'spectral-lobster'
 };
 
 
