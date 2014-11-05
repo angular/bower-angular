a957ecac2b0f34c5dbda835d47ccc9de54948943
jenkins <ci-bot@angularjs.org>
2014-11-05 10:34:53 -0800

    v1.3.2-build.3521+sha.74981c9

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.2-build.3520+sha.dc4b065
+ * @license AngularJS v1.3.2-build.3521+sha.74981c9
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.2-build.3520+sha.dc4b065/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.2-build.3521+sha.74981c9/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2089,7 +2089,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.2-build.3520+sha.dc4b065',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.2-build.3521+sha.74981c9',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 2,
@@ -13299,6 +13299,10 @@ function $RootScopeProvider() {
          expect(child.salutation).toEqual('Welcome');
          expect(parent.salutation).toEqual('Hello');
      * ```
+     *
+     * When interacting with `Scope` in tests, additional helper methods are available on the
+     * instances of `Scope` type. See {@link ngMock.$rootScope.Scope ngMock Scope} for additional
+     * details.
      *
      *
      * @param {Object.<string, function()>=} providers Map of service factory which need to be
