dfcedbe155b0e4c3d986aa711c0bf9c0b3dc1aa7
jenkins <ci-bot@angularjs.org>
2014-11-10 10:34:40 -0800

    v1.3.3-build.3542+sha.41b36e6

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.3-build.3541+sha.52545e5
+ * @license AngularJS v1.3.3-build.3542+sha.41b36e6
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3541+sha.52545e5/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3542+sha.41b36e6/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2089,7 +2089,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.3-build.3541+sha.52545e5',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.3-build.3542+sha.41b36e6',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 3,
@@ -5827,7 +5827,7 @@ function $TemplateCacheProvider() {
  *
  *
  * #### `bindToController`
- * When an isolate scope is used for a component (see above), and `controllerAs` is used, `bindToController` will
+ * When an isolate scope is used for a component (see above), and `controllerAs` is used, `bindToController: true` will
  * allow a component to have its properties bound to the controller, rather than to scope. When the controller
  * is instantiated, the initial values of the isolate scope bindings are already available.
  *
