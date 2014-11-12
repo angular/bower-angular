da8ab19592499ad296357288581c7aa3cd315cf0
jenkins <ci-bot@angularjs.org>
2014-11-12 00:34:54 -0800

    v1.3.3-build.3554+sha.fa0d8c4

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.3-build.3553+sha.7e233eb
+ * @license AngularJS v1.3.3-build.3554+sha.fa0d8c4
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3553+sha.7e233eb/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3554+sha.fa0d8c4/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2089,7 +2089,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.3-build.3553+sha.7e233eb',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.3-build.3554+sha.fa0d8c4',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 3,
@@ -8296,6 +8296,10 @@ function $ControllerProvider() {
      *    * check if evaluating the string on the current scope returns a constructor
      *    * if $controllerProvider#allowGlobals, check `window[constructor]` on the global
      *      `window` object (not recommended)
+     *
+     *    The string can use the `controller as property` syntax, where the controller instance is published
+     *    as the specified property on the `scope`; the `scope` must be injected into `locals` param for this
+     *    to work correctly.
      *
      * @param {Object} locals Injection locals for Controller.
      * @return {Object} Instance of given controller.
