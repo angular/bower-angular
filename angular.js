aa53636e4c4573cf3b9e57a7dedd4076c046cada
jenkins <ci-bot@angularjs.org>
2014-11-13 06:10:09 -0800

    v1.3.3-build.3559+sha.14ff529

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.3-build.3558+sha.fbad280
+ * @license AngularJS v1.3.3-build.3559+sha.14ff529
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3558+sha.fbad280/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.3-build.3559+sha.14ff529/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -1002,7 +1002,7 @@ function bind(self, fn) {
     return curryArgs.length
       ? function() {
           return arguments.length
-            ? fn.apply(self, curryArgs.concat(slice.call(arguments, 0)))
+            ? fn.apply(self, concat(curryArgs, arguments, 0))
             : fn.apply(self, curryArgs);
         }
       : function() {
@@ -2089,7 +2089,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.3-build.3558+sha.fbad280',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.3-build.3559+sha.14ff529',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 3,
