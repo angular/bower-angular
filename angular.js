e1aee2e6f976b6885973b5325a190fd6b68b8f9c
jenkins <ci-bot@angularjs.org>
2014-11-22 08:00:15 -0800

    v1.3.4-build.3602+sha.764fa86

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.4-build.3601+sha.7812dfc
+ * @license AngularJS v1.3.4-build.3602+sha.764fa86
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.4-build.3601+sha.7812dfc/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.4-build.3602+sha.764fa86/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2100,7 +2100,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.4-build.3601+sha.7812dfc',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.4-build.3602+sha.764fa86',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 4,
@@ -4975,7 +4975,7 @@ function Browser(window, document, $log, $sniffer) {
       // IE<10 from getting into redirect loop when in LocationHashbangInHtml5Url mode.
       // See https://github.com/angular/angular.js/commit/ffb2701
       if (lastBrowserUrl === url && (!$sniffer.history || sameState)) {
-        return;
+        return self;
       }
       var sameBase = lastBrowserUrl && stripHash(lastBrowserUrl) === stripHash(url);
       lastBrowserUrl = url;
