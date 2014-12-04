60325c21394b1735500970561930641818b231fc
jenkins <ci-bot@angularjs.org>
2014-12-04 12:37:54 -0800

    v1.3.6-build.3665+sha.8c3a42c

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.6-build.3664+sha.96e7897
+ * @license AngularJS v1.3.6-build.3665+sha.8c3a42c
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.6-build.3664+sha.96e7897/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.6-build.3665+sha.8c3a42c/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2104,7 +2104,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.6-build.3664+sha.96e7897',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.6-build.3665+sha.8c3a42c',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 6,
@@ -15965,10 +15965,8 @@ function $TemplateRequestProvider() {
 
       return $http.get(tpl, httpOptions)
         .then(function(response) {
-          var html = response.data;
           self.totalPendingRequests--;
-          $templateCache.put(tpl, html);
-          return html;
+          return response.data;
         }, handleError);
 
       function handleError(resp) {
