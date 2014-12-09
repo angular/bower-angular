da996192e8b7705954bcca77b1d8e29d25b1616d
jenkins <ci-bot@angularjs.org>
2014-12-08 16:10:58 -0800

    v1.3.36

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.6-build.3681+sha.6ad109e
+ * @license AngularJS v1.3.36
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.6-build.3681+sha.6ad109e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.36/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2105,11 +2105,11 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.6-build.3681+sha.6ad109e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.36',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
-  dot: 6,
-  codeName: 'snapshot'
+  dot: 36,
+  codeName: 'robofunky-danceblaster'
 };
 
 
