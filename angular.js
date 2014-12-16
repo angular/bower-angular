beac0fff3d0e8bc6010c2ab9c2392c19735cbb51
jenkins <ci-bot@angularjs.org>
2014-12-16 14:21:53 -0800

    v1.3.8-build.3707+sha.cd77c08

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.8-build.3706+sha.cd77c08
+ * @license AngularJS v1.3.8-build.3707+sha.cd77c08
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.8-build.3706+sha.cd77c08/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.8-build.3707+sha.cd77c08/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2118,7 +2118,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.8-build.3706+sha.cd77c08',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.8-build.3707+sha.cd77c08',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 8,
