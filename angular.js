733b1c5f78b04b04016b7d9315c55550364f3025
jenkins <ci-bot@angularjs.org>
2014-12-23 11:31:55 -0800

    v1.2.29-build.587+sha.d60fbcc

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.29-build.586+sha.a4a4b82
+ * @license AngularJS v1.2.29-build.587+sha.d60fbcc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.29-build.586+sha.a4a4b82/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.29-build.587+sha.d60fbcc/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1989,7 +1989,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.29-build.586+sha.a4a4b82',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.29-build.587+sha.d60fbcc',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 29,
