5fb93fbacaf6ce5ef45ba13dcd1130729cfe2b1e
jenkins <ci-bot@angularjs.org>
2014-12-28 09:53:58 -0800

    v1.3.9-build.3733+sha.4836dac

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.3732+sha.0e2ac3c
+ * @license AngularJS v1.3.9-build.3733+sha.4836dac
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3732+sha.0e2ac3c/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3733+sha.4836dac/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2118,7 +2118,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.9-build.3732+sha.0e2ac3c',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.9-build.3733+sha.4836dac',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 9,
@@ -8893,7 +8893,7 @@ function $HttpProvider() {
      * To add or overwrite these defaults, simply add or remove a property from these configuration
      * objects. To add headers for an HTTP method other than POST or PUT, simply add a new object
      * with the lowercased HTTP method name as the key, e.g.
-     * `$httpProvider.defaults.headers.get = { 'My-Header' : 'value' }.
+     * `$httpProvider.defaults.headers.get = { 'My-Header' : 'value' }`.
      *
      * The defaults can also be set at runtime via the `$http.defaults` object in the same
      * fashion. For example:
