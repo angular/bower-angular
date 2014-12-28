1538a0b04ff7d3c8ce40670efe9a49d8e9cced94
jenkins <ci-bot@angularjs.org>
2014-12-28 12:24:33 -0800

    v1.3.9-build.3736+sha.d224fe8

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.3735+sha.e9bf93d
+ * @license AngularJS v1.3.9-build.3736+sha.d224fe8
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3735+sha.e9bf93d/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3736+sha.d224fe8/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2118,7 +2118,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.9-build.3735+sha.e9bf93d',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.9-build.3736+sha.d224fe8',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 9,
@@ -19125,7 +19125,7 @@ var inputType = {
         }]);
      </script>
      <form name="myForm" ng-controller="DateController as dateCtrl">
-       Pick a month int 2013:
+       Pick a month in 2013:
        <input id="exampleInput" type="month" name="input" ng-model="value"
           placeholder="yyyy-MM" min="2013-01" max="2013-12" required />
        <span class="error" ng-show="myForm.input.$error.required">
