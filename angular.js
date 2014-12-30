e01f7ba6156f7208f70d167555a7185fd5781b7b
jenkins <ci-bot@angularjs.org>
2014-12-30 10:06:58 -0800

    v1.3.9-build.3740+sha.1c76bf7

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.3739+sha.6018f5d
+ * @license AngularJS v1.3.9-build.3740+sha.1c76bf7
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3739+sha.6018f5d/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3740+sha.1c76bf7/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2118,7 +2118,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.9-build.3739+sha.6018f5d',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.9-build.3740+sha.1c76bf7',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 9,
