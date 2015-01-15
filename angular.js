3e2cfa2c13ee95abe9be9903cbb060c5d526666d
jenkins <ci-bot@angularjs.org>
2015-01-15 06:49:50 -0800

    v1.3.10-build.19+sha.3a799df

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.10-build.18+sha.3a799df
+ * @license AngularJS v1.3.10-build.19+sha.3a799df
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.10-build.18+sha.3a799df/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.10-build.19+sha.3a799df/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2118,7 +2118,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.10-build.18+sha.3a799df',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.10-build.19+sha.3a799df',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 10,
