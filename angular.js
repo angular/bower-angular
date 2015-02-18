5c4bd518f7e270e84069e60921205d45ab7702d3
jenkins <ci-bot@angularjs.org>
2015-02-18 05:43:21 -0800

    v1.4.0-build.3840+sha.30b4813

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3839+sha.da9eac8
+ * @license AngularJS v1.4.0-build.3840+sha.30b4813
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-build.3839+sha.da9eac8/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3840+sha.30b4813/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2195,7 +2195,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.0-build.3839+sha.da9eac8',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.0-build.3840+sha.30b4813',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 0,
@@ -26863,6 +26863,7 @@ var SelectController =
       if (value === '') self.emptyOption.prop('selected', true); // to make IE9 happy
     } else {
       if (isUndefined(value) && self.emptyOption) {
+        self.removeUnknownOption();
         $element.val('');
       } else {
         self.renderUnknownOption(value);
