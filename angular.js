77788f9a1ec62fc601d9e92810dca4eef0d659ff
jenkins <ci-bot@angularjs.org>
2015-03-02 06:05:18 -0800

    v1.3.15-build.64+sha.b9ad91c

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.15-build.63+sha.190ea88
+ * @license AngularJS v1.3.15-build.64+sha.b9ad91c
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.15-build.63+sha.190ea88/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.15-build.64+sha.b9ad91c/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2122,7 +2122,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.15-build.63+sha.190ea88',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.15-build.64+sha.b9ad91c',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 15,
