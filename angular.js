60112301f80d9a600c87dcff048662fc24c327ac
jenkins <ci-bot@angularjs.org>
2015-03-10 05:44:30 -0700

    v1.4.0-build.3892+sha.1924cf2

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3891+sha.fb7db4a
+ * @license AngularJS v1.4.0-build.3892+sha.1924cf2
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-build.3891+sha.fb7db4a/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3892+sha.1924cf2/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -431,8 +431,6 @@ function baseExtend(dst, objs, deep) {
  *
  * @param {Object} dst Destination object.
  * @param {...Object} src Source object(s).
- * @param {boolean=} deep if the last parameter is set to `true`, objects are recursively merged
- *    (deep copy). Defaults to `false`.
  * @returns {Object} Reference to `dst`.
  */
 function extend(dst) {
@@ -2262,7 +2260,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.0-build.3891+sha.fb7db4a',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.0-build.3892+sha.1924cf2',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 0,
