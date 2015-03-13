00f7dfb8185cfe3ea70359868a8c264b36f32311
jenkins <ci-bot@angularjs.org>
2015-03-13 10:13:50 -0700

    v1.4.0-build.3903+sha.2f4be3e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3902+sha.3086fcd
+ * @license AngularJS v1.4.0-build.3903+sha.2f4be3e
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-build.3902+sha.3086fcd/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3903+sha.2f4be3e/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2223,6 +2223,7 @@ function toDebugString(obj) {
   $FilterProvider,
   $InterpolateProvider,
   $IntervalProvider,
+  $$HashMapProvider,
   $HttpProvider,
   $HttpBackendProvider,
   $LocationProvider,
@@ -2262,7 +2263,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.0-build.3902+sha.3086fcd',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.0-build.3903+sha.2f4be3e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 0,
@@ -2399,6 +2400,7 @@ function publishExternalAPI(angular) {
         $$rAF: $$RAFProvider,
         $$asyncCallback: $$AsyncCallbackProvider,
         $$jqLite: $$jqLiteProvider,
+        $$HashMap: $$HashMapProvider,
         $$cookieReader: $$CookieReaderProvider
       });
     }
@@ -3507,6 +3509,12 @@ HashMap.prototype = {
     return value;
   }
 };
+
+var $$HashMapProvider = [function() {
+  this.$get = [function() {
+    return HashMap;
+  }];
+}];
 
 /**
  * @ngdoc function
