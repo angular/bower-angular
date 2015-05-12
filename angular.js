1c24f5eddb987010770561280307c2d0dafc7cd8
jenkins <ci-bot@angularjs.org>
2015-05-12 12:42:52 -0700

    v1.4.0-build.3996+sha.13e38db

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-rc.2
+ * @license AngularJS v1.4.0-build.3996+sha.13e38db
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-rc.2/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3996+sha.13e38db/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2332,11 +2332,11 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.0-rc.2',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.0-build.3996+sha.13e38db',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 0,
-  codeName: 'rocket-zambonimation'
+  codeName: 'snapshot'
 };
 
 
