b37cc01ae41678df898abe9fa8c0d3b8480958f8
jenkins <ci-bot@angularjs.org>
2015-06-05 13:45:16 -0700

    v1.3.16-build.120+sha.5a1b4a0

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.16-build.119+sha.706a93a
+ * @license AngularJS v1.3.16-build.120+sha.5a1b4a0
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.16-build.119+sha.706a93a/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.16-build.120+sha.5a1b4a0/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2139,7 +2139,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.16-build.119+sha.706a93a',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.16-build.120+sha.5a1b4a0',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 16,
