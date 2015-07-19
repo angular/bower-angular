a8ba3f4692b647af6cfc6582a356f8bebcad8086
jenkins <ci-bot@angularjs.org>
2015-07-19 08:56:58 -0700

    v1.4.4-build.4120+sha.cce084e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.4-build.4119+sha.d935c24
+ * @license AngularJS v1.4.4-build.4120+sha.cce084e
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.4-build.4119+sha.d935c24/' +
+    message += '\nhttp://errors.angularjs.org/1.4.4-build.4120+sha.cce084e/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2365,7 +2365,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.4-build.4119+sha.d935c24',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.4-build.4120+sha.cce084e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 4,
