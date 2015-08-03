c925f1220abf8d6de34a1dc1b7f6cc34f09aaaa3
jenkins <ci-bot@angularjs.org>
2015-08-03 01:56:37 -0700

    v1.4.4-build.4145+sha.5abf593

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.4-build.4144+sha.9efe60f
+ * @license AngularJS v1.4.4-build.4145+sha.5abf593
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.4-build.4144+sha.9efe60f/' +
+    message += '\nhttp://errors.angularjs.org/1.4.4-build.4145+sha.5abf593/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2373,7 +2373,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.4-build.4144+sha.9efe60f',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.4-build.4145+sha.5abf593',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 4,
@@ -4362,6 +4362,7 @@ function createInjector(modulesToLoad, strictDi) {
   // Module Loading
   ////////////////////////////////////
   function loadModules(modulesToLoad) {
+    assertArg(isUndefined(modulesToLoad) || isArray(modulesToLoad), 'modulesToLoad', 'not an array');
     var runBlocks = [], moduleFn;
     forEach(modulesToLoad, function(module) {
       if (loadedModules.get(module)) return;
