afd2be71bd2c4395228eff51e1ef3e04ae8ef7d5
jenkins <ci-bot@angularjs.org>
2015-08-24 08:19:58 -0700

    v1.4.5-build.4187+sha.0e00108

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.5-build.4186+sha.85e3203
+ * @license AngularJS v1.4.5-build.4187+sha.0e00108
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.5-build.4186+sha.85e3203/' +
+    message += '\nhttp://errors.angularjs.org/1.4.5-build.4187+sha.0e00108/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2374,7 +2374,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.5-build.4186+sha.85e3203',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.5-build.4187+sha.0e00108',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 5,
@@ -28491,8 +28491,9 @@ var patternDirective = function() {
         ctrl.$validate();
       });
 
-      ctrl.$validators.pattern = function(value) {
-        return ctrl.$isEmpty(value) || isUndefined(regexp) || regexp.test(value);
+      ctrl.$validators.pattern = function(modelValue, viewValue) {
+        // HTML5 pattern constraint validates the input value, so we validate the viewValue
+        return ctrl.$isEmpty(viewValue) || isUndefined(regexp) || regexp.test(viewValue);
       };
     }
   };
