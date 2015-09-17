482103a626013304868f53f1dfc65e1f62b39dc1
jenkins <ci-bot@angularjs.org>
2015-09-17 06:09:27 -0700

    v1.5.0-build.4243+sha.f8cf28a

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.7-build.4242+sha.4dd10fd
+ * @license AngularJS v1.5.0-build.4243+sha.f8cf28a
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.7-build.4242+sha.4dd10fd/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4243+sha.f8cf28a/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2378,10 +2378,10 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.7-build.4242+sha.4dd10fd',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4243+sha.f8cf28a',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
-  minor: 4,
-  dot: 7,
+  minor: 5,
+  dot: 0,
   codeName: 'snapshot'
 };
 
