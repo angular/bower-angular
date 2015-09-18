1a3da2f2dbae7da06017d23742573d1e958ae9b3
jenkins <ci-bot@angularjs.org>
2015-09-18 02:49:22 -0700

    v1.5.0-build.4244+sha.f8cf28a

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-beta.0
+ * @license AngularJS v1.5.0-build.4244+sha.f8cf28a
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-beta.0/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4244+sha.f8cf28a/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2378,11 +2378,11 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-beta.0',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4244+sha.f8cf28a',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
-  codeName: 'intialization-processation'
+  codeName: 'snapshot'
 };
 
 
