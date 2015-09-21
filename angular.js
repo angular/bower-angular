13ed7b2391f90c04afd3e9ca68d85e7b88579733
jenkins <ci-bot@angularjs.org>
2015-09-21 12:24:47 -0700

    v1.5.0-build.4263+sha.7dcfe5e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4262+sha.9e83b83
+ * @license AngularJS v1.5.0-build.4263+sha.7dcfe5e
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4262+sha.9e83b83/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4263+sha.7dcfe5e/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2356,7 +2356,6 @@ function toDebugString(obj) {
   $TemplateCacheProvider,
   $TemplateRequestProvider,
   $$TestabilityProvider,
-  $TimeProvider,
   $TimeoutProvider,
   $$RAFProvider,
   $WindowProvider,
@@ -2381,7 +2380,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4262+sha.9e83b83',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4263+sha.7dcfe5e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
@@ -2515,7 +2514,6 @@ function publishExternalAPI(angular) {
         $templateCache: $TemplateCacheProvider,
         $templateRequest: $TemplateRequestProvider,
         $$testability: $$TestabilityProvider,
-        $date: $DateProvider,
         $timeout: $TimeoutProvider,
         $window: $WindowProvider,
         $$rAF: $$RAFProvider,
@@ -17840,50 +17838,6 @@ function $$TestabilityProvider() {
     };
 
     return testability;
-  }];
-}
-
-/**
- * @ngdoc service
- * @name $date
- * @requires $window
- *
- * @description
- * Simple service for accessing date.
- *
- * The main purpose of this service is to simplify mocking date in tests.
- *
- * @example
-   <example module="dateExample">
-     <file name="script.js">
-       angular.module('dateExample', [])
-         .controller('TimeController', ['$scope', '$date', function($scope, $date) {
-           $scope.now = $date.now();
-         }]);
-     </file>
-     <file name="index.html">
-       <div ng-controller="TimeController">
-         <p>Time when the page was created: {{now | date}}</p>
-       </div>
-     </file>
-   </example>
- */
-function $DateProvider() {
-  var self = this;
-
-  this.$get = ['$window', function($window) {
-    return {
-      /**
-       * @ngdoc method
-       * @name $date#now
-       *
-       * @description
-       * Return Date object representing current date
-       */
-      now: function() {
-        return $window.Date();
-      }
-    };
   }];
 }
 
