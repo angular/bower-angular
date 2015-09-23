032e94ab4a6968434486d7023be99addce3c9438
jenkins <ci-bot@angularjs.org>
2015-09-23 13:46:40 -0700

    v1.3.20-build.150+sha.bce1d8e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.20-build.149+sha.5a1bc6e
+ * @license AngularJS v1.3.20-build.150+sha.bce1d8e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -54,7 +54,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.20-build.149+sha.5a1bc6e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.20-build.150+sha.bce1d8e/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -2139,7 +2139,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.20-build.149+sha.5a1bc6e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.20-build.150+sha.bce1d8e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 20,
