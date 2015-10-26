8d5a0bdcff5a2d18f3a5c9a6639661d90c858dc2
jenkins <ci-bot@angularjs.org>
2015-10-26 10:07:50 -0700

    v1.5.0-build.4329+sha.773efd0

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4328+sha.8088194
+ * @license AngularJS v1.5.0-build.4329+sha.773efd0
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4328+sha.8088194/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4329+sha.773efd0/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -260,11 +260,6 @@ var
 msie = document.documentMode;
 
 
-function isNodeList(obj) {
-  return typeof obj.length == 'number' &&
-         typeof obj.item == 'function';
-}
-
 /**
  * @private
  * @param {*} obj
@@ -276,15 +271,20 @@ function isArrayLike(obj) {
   // `null`, `undefined` and `window` are not array-like
   if (obj == null || isWindow(obj)) return false;
 
-  // arrays and strings are array like
-  if (isArray(obj) || isString(obj)) return true;
+  // arrays, strings and jQuery/jqLite objects are array like
+  // * jqLite is either the jQuery or jqLite constructor function
+  // * we have to check the existance of jqLite first as this method is called
+  //   via the forEach method when constructing the jqLite object in the first place
+  if (isArray(obj) || isString(obj) || (jqLite && obj instanceof jqLite)) return true;
 
   // Support: iOS 8.2 (not reproducible in simulator)
   // "length" in obj used to prevent JIT error (gh-11508)
   var length = "length" in Object(obj) && obj.length;
 
-  // node lists and objects with suitable length characteristics are array-like
-  return (isNumber(length) && length >= 0 && (length - 1) in obj) || isNodeList(obj);
+  // NodeList objects (with `item` method) and
+  // other objects with suitable length characteristics are array-like
+  return isNumber(length) &&
+    (length >= 0 && (length - 1) in obj || typeof obj.item == 'function');
 }
 
 /**
@@ -2395,7 +2395,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4328+sha.8088194',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4329+sha.773efd0',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
