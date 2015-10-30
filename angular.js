9b9dd16e3fd06d40d85b40f5d6177e5a7bcea16f
jenkins <ci-bot@angularjs.org>
2015-10-30 04:48:43 -0700

    v1.5.0-build.4354+sha.91c0b36

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4353+sha.c8768d1
+ * @license AngularJS v1.5.0-build.4354+sha.91c0b36
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4353+sha.c8768d1/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4354+sha.91c0b36/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2508,7 +2508,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4353+sha.c8768d1',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4354+sha.91c0b36',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
@@ -10439,7 +10439,7 @@ function $HttpProvider() {
      */
     function $http(requestConfig) {
 
-      if (!angular.isObject(requestConfig)) {
+      if (!isObject(requestConfig)) {
         throw minErr('$http')('badreq', 'Http request configuration must be an object.  Received: {0}', requestConfig);
       }
 
