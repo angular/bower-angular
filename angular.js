7116c5550d3d23e7f7071737479a4bdf36163f4c
jenkins <ci-bot@angularjs.org>
2015-11-18 00:42:56 -0800

    v1.5.0-build.4389+sha.2e23a3c

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-beta.2
+ * @license AngularJS v1.5.0-build.4389+sha.2e23a3c
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-beta.2/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4389+sha.2e23a3c/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2507,11 +2507,11 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-beta.2',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4389+sha.2e23a3c',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
-  codeName: 'effective-delegation'
+  codeName: 'snapshot'
 };
 
 
