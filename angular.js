bc2e445156200c8a94a2b47dc254114143da9f34
jenkins <ci-bot@angularjs.org>
2015-12-08 14:12:33 -0800

    v1.5.0-build.4429+sha.21ab829

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4428+sha.1dd206e
+ * @license AngularJS v1.5.0-build.4429+sha.21ab829
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4428+sha.1dd206e/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4429+sha.21ab829/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2508,7 +2508,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4428+sha.1dd206e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4429+sha.21ab829',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
