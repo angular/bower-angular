24ddb49dddf3b3b6ae8af064288f568989a48c20
jenkins <ci-bot@angularjs.org>
2015-12-10 05:59:58 -0800

    v1.5.0-build.4437+sha.8f0b482

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4436+sha.20604e7
+ * @license AngularJS v1.5.0-build.4437+sha.8f0b482
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4436+sha.20604e7/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4437+sha.8f0b482/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2508,7 +2508,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4436+sha.20604e7',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4437+sha.8f0b482',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
@@ -27404,7 +27404,7 @@ var ngOptionsDirective = ['$compile', '$parse', function($compile, $parse) {
                 (current === emptyOption_ ||
                 current === unknownOption_ ||
                 current.nodeType === NODE_TYPE_COMMENT ||
-                current.value === '')) {
+                (nodeName_(current) === 'option' && current.value === ''))) {
             current = current.nextSibling;
           }
         }
