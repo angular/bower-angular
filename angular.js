4abdda11968737be1cab4a28ea2d3a770668a9ab
jenkins <ci-bot@angularjs.org>
2015-12-18 05:11:30 -0800

    v1.5.0-build.4459+sha.dec8a0e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4458+sha.9877648
+ * @license AngularJS v1.5.0-build.4459+sha.dec8a0e
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4458+sha.9877648/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4459+sha.dec8a0e/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2551,7 +2551,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4458+sha.9877648',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4459+sha.dec8a0e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
@@ -14744,9 +14744,6 @@ Parser.prototype = {
     return this.astCompiler.compile(text, this.options.expensiveChecks);
   }
 };
-
-var getterFnCacheDefault = createMap();
-var getterFnCacheExpensive = createMap();
 
 function isPossiblyDangerousMemberName(name) {
   return name == 'constructor';
