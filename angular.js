d27b1f6dab0df6bd04ffbaf315e9393ac023794c
jenkins <ci-bot@angularjs.org>
2015-12-20 13:53:50 -0800

    v1.5.0-build.4462+sha.798fb18

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4461+sha.99d601a
+ * @license AngularJS v1.5.0-build.4462+sha.798fb18
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4461+sha.99d601a/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4462+sha.798fb18/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2207,7 +2207,7 @@ function setupModuleLoader(window) {
            *      returns an html template as a string which should be used as the contents of this component.
            *      Empty string by default.
            *
-           *      If `template` is a function, then it is {@link auto.$injector#invoke injected} with
+           *      If `template` is a function, then it is {@link guide/di injectable}, and receives
            *      the following locals:
            *
            *      - `$element` - Current element
@@ -2216,7 +2216,7 @@ function setupModuleLoader(window) {
            *    - `templateUrl` – `{string=|function()=}` – path or function that returns a path to an html
            *      template that should be used  as the contents of this component.
            *
-           *      If `templateUrl` is a function, then it is {@link auto.$injector#invoke injected} with
+           *      If `templateUrl` is a function, then it is {@link guide/di injectable}, and receives
            *      the following locals:
            *
            *      - `$element` - Current element
@@ -2551,7 +2551,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4461+sha.99d601a',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4462+sha.798fb18',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
