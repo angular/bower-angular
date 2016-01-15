ef7d0e8bf6a33d44fe5d3b4d02c17d84d894b0ba
jenkins <ci-bot@angularjs.org>
2016-01-15 14:27:55 -0800

    v1.5.0-rc.1

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4507+sha.1358719
+ * @license AngularJS v1.5.0-rc.1
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4507+sha.1358719/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-rc.1/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2441,11 +2441,11 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4507+sha.1358719',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-rc.1',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
-  codeName: 'snapshot'
+  codeName: 'quantum-fermentation'
 };
 
 
