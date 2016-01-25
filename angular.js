5c554a3519a5af32a23f69b9822e02dfece3175f
jenkins <ci-bot@angularjs.org>
2016-01-25 09:15:36 -0800

    v1.5.0-build.4540+sha.97e97d1

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4539+sha.99eeec3
+ * @license AngularJS v1.5.0-build.4540+sha.97e97d1
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.0-build.4539+sha.99eeec3/' +
+    message += '\nhttp://errors.angularjs.org/1.5.0-build.4540+sha.97e97d1/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2420,7 +2420,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.0-build.4539+sha.99eeec3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.0-build.4540+sha.97e97d1',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 0,
@@ -20042,7 +20042,7 @@ function dateFilter($locale) {
     forEach(parts, function(value) {
       fn = DATE_FORMATS[value];
       text += fn ? fn(date, $locale.DATETIME_FORMATS, dateTimezoneOffset)
-                 : value.replace(/(^'|'$)/g, '').replace(/''/g, "'");
+                 : value === "''" ? "'" : value.replace(/(^'|'$)/g, '').replace(/''/g, "'");
     });
 
     return text;
