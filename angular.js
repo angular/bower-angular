e2bc40635a964bd72e0c444361cf1804e2035c1a
jenkins <ci-bot@angularjs.org>
2016-02-23 14:50:14 -0800

    v1.5.1-build.4641+sha.76f47d5

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.1-build.4639+sha.7e112c1
+ * @license AngularJS v1.5.1-build.4641+sha.76f47d5
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.1-build.4639+sha.7e112c1/' +
+    message += '\nhttp://errors.angularjs.org/1.5.1-build.4641+sha.76f47d5/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2443,7 +2443,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.1-build.4639+sha.7e112c1',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.1-build.4641+sha.76f47d5',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 1,
@@ -9760,6 +9760,15 @@ function identifierForController(controller, ident) {
 function $ControllerProvider() {
   var controllers = {},
       globals = false;
+
+  /**
+   * @ngdoc method
+   * @name $controllerProvider#has
+   * @param {string} name Controller name to check.
+   */
+  this.has = function(name) {
+    return controllers.hasOwnProperty(name);
+  };
 
   /**
    * @ngdoc method
