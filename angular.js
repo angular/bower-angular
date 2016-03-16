49f7c72f885619ddd93a6a30c55b9fb9da542a2c
jenkins <ci-bot@angularjs.org>
2016-03-16 06:34:37 -0700

    v1.5.1

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.1-build.4667+sha.d2cc451
+ * @license AngularJS v1.5.1
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.1-build.4667+sha.d2cc451/' +
+    message += '\nhttp://errors.angularjs.org/1.5.1/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2443,11 +2443,11 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.1-build.4667+sha.d2cc451',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.1',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 1,
-  codeName: 'snapshot'
+  codeName: 'equivocal-sophistication'
 };
 
 
