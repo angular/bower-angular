c11183de149294fdd9a4edc6537e14d0cded5af0
jenkins <ci-bot@angularjs.org>
2016-06-13 03:37:33 -0700

    v1.5.7-build.4871+sha.3ddf2f6

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.7-build.4870+sha.9f68b0e
+ * @license AngularJS v1.5.7-build.4871+sha.3ddf2f6
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.7-build.4870+sha.9f68b0e/' +
+    message += '\nhttp://errors.angularjs.org/1.5.7-build.4871+sha.3ddf2f6/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2511,7 +2511,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.7-build.4870+sha.9f68b0e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.7-build.4871+sha.3ddf2f6',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 7,
@@ -10336,7 +10336,7 @@ function $$IsDocumentHiddenProvider() {
  *         logErrorsToBackend(exception, cause);
  *         $log.warn(exception, cause);
  *       };
- *     });
+ *     }]);
  * ```
  *
  * <hr />
