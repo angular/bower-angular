9087e37887fa05f6b756173f66b9e2bf132dc5f5
jenkins <ci-bot@angularjs.org>
2016-07-05 12:15:11 -0700

    v1.5.8-build.4923+sha.b928043

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.8-build.4922+sha.a2c1675
+ * @license AngularJS v1.5.8-build.4923+sha.b928043
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.8-build.4922+sha.a2c1675/' +
+    message += '\nhttp://errors.angularjs.org/1.5.8-build.4923+sha.b928043/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2541,7 +2541,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.8-build.4922+sha.a2c1675',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.8-build.4923+sha.b928043',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 8,
