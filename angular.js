710ac335c6031c7c73d54516ebcefb75159f0372
jenkins <ci-bot@angularjs.org>
2016-08-08 09:47:43 -0700

    v1.5.9-build.4983+sha.fdf8e0f

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.9-build.4982+sha.ec565dd
+ * @license AngularJS v1.5.9-build.4983+sha.fdf8e0f
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.9-build.4982+sha.ec565dd/' +
+    message += '\nhttp://errors.angularjs.org/1.5.9-build.4983+sha.fdf8e0f/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2546,7 +2546,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.9-build.4982+sha.ec565dd',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.9-build.4983+sha.fdf8e0f',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 9,
@@ -12139,6 +12139,7 @@ function createHttpBackend($browser, createXhr, $browserDefer, callbacks, rawDoc
 
       xhr.onerror = requestError;
       xhr.onabort = requestError;
+      xhr.ontimeout = requestError;
 
       forEach(eventHandlers, function(value, key) {
           xhr.addEventListener(key, value);
