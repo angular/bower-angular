4f9dcd160625fb957b2a7d7d3c65f30ba84bc4f3
jenkins <ci-bot@angularjs.org>
2016-08-26 09:54:36 -0700

    v1.5.9-build.5005+sha.e3a378e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.9-build.5004+sha.c75698d
+ * @license AngularJS v1.5.9-build.5005+sha.e3a378e
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.9-build.5004+sha.c75698d/' +
+    message += '\nhttp://errors.angularjs.org/1.5.9-build.5005+sha.e3a378e/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2548,7 +2548,7 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.5.9-build.5004+sha.c75698d',
+  full: '1.5.9-build.5005+sha.e3a378e',
   major: 1,
   minor: 5,
   dot: 9,
