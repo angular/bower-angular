7e21bdf3559030d1012204bc633613abbb860d49
jenkins <ci-bot@angularjs.org>
2016-10-07 13:35:59 -0700

    v1.5.9-build.5051+sha.f41bd76

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.9-build.5050+sha.b3a3ed3
+ * @license AngularJS v1.5.9-build.5051+sha.f41bd76
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.9-build.5050+sha.b3a3ed3/' +
+    message += '\nhttp://errors.angularjs.org/1.5.9-build.5051+sha.f41bd76/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2555,7 +2555,7 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.5.9-build.5050+sha.b3a3ed3',
+  full: '1.5.9-build.5051+sha.f41bd76',
   major: 1,
   minor: 5,
   dot: 9,
