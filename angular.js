54db0fb33b05aea872ab74fdbc97e95c013ff58f
jenkins <ci-bot@angularjs.org>
2016-10-17 03:47:58 -0700

    v1.5.9-build.5067+sha.705afcd

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.9-build.5066+sha.aa6a806
+ * @license AngularJS v1.5.9-build.5067+sha.705afcd
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.9-build.5066+sha.aa6a806/' +
+    message += '\nhttp://errors.angularjs.org/1.5.9-build.5067+sha.705afcd/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2555,7 +2555,7 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.5.9-build.5066+sha.aa6a806',
+  full: '1.5.9-build.5067+sha.705afcd',
   major: 1,
   minor: 5,
   dot: 9,
@@ -13119,8 +13119,8 @@ function parseAppUrl(relativeUrl, locationObj) {
   }
 }
 
-function startsWith(haystack, needle) {
-  return haystack.lastIndexOf(needle, 0) === 0;
+function startsWith(str, search) {
+  return str.slice(0, search.length) === search;
 }
 
 /**
