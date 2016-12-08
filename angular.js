8f78800049c7322870e0ee560f748a8126570641
jenkins <ci-bot@angularjs.org>
2016-12-08 04:41:17 -0800

    v1.6.1-build.5188+sha.1b7ddd3

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.0
+ * @license AngularJS v1.6.1-build.5188+sha.1b7ddd3
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.0/' +
+    message += '\nhttp://errors.angularjs.org/1.6.1-build.5188+sha.1b7ddd3/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2623,11 +2623,11 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.0',
+  full: '1.6.1-build.5188+sha.1b7ddd3',
   major: 1,
   minor: 6,
-  dot: 0,
-  codeName: 'rainbow-tsunami'
+  dot: 1,
+  codeName: 'snapshot'
 };
 
 
