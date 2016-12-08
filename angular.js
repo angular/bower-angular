a4bb60ab4fe903c77120aafc37f341a2b9b6be7d
jenkins <ci-bot@angularjs.org>
2016-12-08 03:12:00 -0800

    v1.5.10-build.5186+sha.f0dc288

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.10-build.5185+sha.70490ef
+ * @license AngularJS v1.5.10-build.5186+sha.f0dc288
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.10-build.5185+sha.70490ef/' +
+    message += '\nhttp://errors.angularjs.org/1.5.10-build.5186+sha.f0dc288/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2623,7 +2623,7 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.5.10-build.5185+sha.70490ef',
+  full: '1.5.10-build.5186+sha.f0dc288',
   major: 1,
   minor: 5,
   dot: 10,
