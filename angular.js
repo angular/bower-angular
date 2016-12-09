a1c19b1514309b9b0d4547a5c3283bb914e160fc
jenkins <ci-bot@angularjs.org>
2016-12-09 02:45:29 -0800

    v1.6.1-build.5198+sha.1ecf6ef

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.1-build.5197+sha.b820970
+ * @license AngularJS v1.6.1-build.5198+sha.1ecf6ef
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.1-build.5197+sha.b820970/' +
+    message += '\nhttp://errors.angularjs.org/1.6.1-build.5198+sha.1ecf6ef/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2623,7 +2623,7 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.1-build.5197+sha.b820970',
+  full: '1.6.1-build.5198+sha.1ecf6ef',
   major: 1,
   minor: 6,
   dot: 1,
