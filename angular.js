76ca7854fb380e13692d118af3d0e57f9197b049
jenkins <ci-bot@angularjs.org>
2016-12-13 15:01:22 -0800

    v1.6.1-build.5204+sha.d03cac4

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.1-build.5203+sha.e9a4de0
+ * @license AngularJS v1.6.1-build.5204+sha.d03cac4
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.1-build.5203+sha.e9a4de0/' +
+    message += '\nhttp://errors.angularjs.org/1.6.1-build.5204+sha.d03cac4/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2623,7 +2623,7 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.1-build.5203+sha.e9a4de0',
+  full: '1.6.1-build.5204+sha.d03cac4',
   major: 1,
   minor: 6,
   dot: 1,
@@ -16528,6 +16528,7 @@ function $QProvider() {
    *
    * @description
    * Retrieves or overrides whether to generate an error when a rejected promise is not handled.
+   * This feature is enabled by default.
    *
    * @param {boolean=} value Whether to generate an error when a rejected promise is not handled.
    * @returns {boolean|ng.$qProvider} Current value when called without a new value or self for
