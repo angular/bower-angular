3828060389c83384b0a162eea82b21f183aa060c
jenkins <ci-bot@angularjs.org>
2016-12-23 02:59:50 -0800

    v1.6.1

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.1-build.5217+sha.8ceeecc
+ * @license AngularJS v1.6.1
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.1-build.5217+sha.8ceeecc/' +
+    message += '\nhttp://errors.angularjs.org/1.6.1/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2623,11 +2623,11 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.1-build.5217+sha.8ceeecc',
+  full: '1.6.1',
   major: 1,
   minor: 6,
   dot: 1,
-  codeName: 'snapshot'
+  codeName: 'promise-rectification'
 };
 
 
