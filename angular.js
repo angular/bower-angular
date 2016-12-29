3fb6f213b2e918c8a7654faef5bc3922e99bedad
jenkins <ci-bot@angularjs.org>
2016-12-29 00:18:59 -0800

    v1.6.2-build.5219+sha.922a83e

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.2-build.5218+sha.ee1458f
+ * @license AngularJS v1.6.2-build.5219+sha.922a83e
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.2-build.5218+sha.ee1458f/' +
+    message += '\nhttp://errors.angularjs.org/1.6.2-build.5219+sha.922a83e/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2623,7 +2623,7 @@ function toDebugString(obj) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.2-build.5218+sha.ee1458f',
+  full: '1.6.2-build.5219+sha.922a83e',
   major: 1,
   minor: 6,
   dot: 2,
