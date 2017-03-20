eeead7dbbb9b319aa1c38e2c6bee320ad4839df5
jenkins <ci-bot@angularjs.org>
2017-03-20 03:54:02 -0700

    v1.6.4-build.5318+sha.24c94b8

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.4-build.5317+sha.0c3620b
+ * @license AngularJS v1.6.4-build.5318+sha.24c94b8
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -56,7 +56,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.4-build.5317+sha.0c3620b/' +
+    message += '\nhttp://errors.angularjs.org/1.6.4-build.5318+sha.24c94b8/' +
       (module ? module + '/' : '') + code;
 
     for (i = 0, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2726,7 +2726,7 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.4-build.5317+sha.0c3620b',
+  full: '1.6.4-build.5318+sha.24c94b8',
   major: 1,
   minor: 6,
   dot: 4,
@@ -2876,7 +2876,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.6.4-build.5317+sha.0c3620b' });
+  .info({ angularVersion: '1.6.4-build.5318+sha.24c94b8' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
