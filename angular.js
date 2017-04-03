a33268fed368a0d7e101654edc03f377a9ee8a46
jenkins <ci-bot@angularjs.org>
2017-04-03 01:42:57 -0700

    v1.6.5-build.5353+sha.136a42a

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.5-build.5352+sha.06516d7
+ * @license AngularJS v1.6.5-build.5353+sha.136a42a
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -56,7 +56,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.5-build.5352+sha.06516d7/' +
+    message += '\nhttp://errors.angularjs.org/1.6.5-build.5353+sha.136a42a/' +
       (module ? module + '/' : '') + code;
 
     for (i = 0, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2731,7 +2731,7 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.5-build.5352+sha.06516d7',
+  full: '1.6.5-build.5353+sha.136a42a',
   major: 1,
   minor: 6,
   dot: 5,
@@ -2881,7 +2881,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.6.5-build.5352+sha.06516d7' });
+  .info({ angularVersion: '1.6.5-build.5353+sha.136a42a' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
