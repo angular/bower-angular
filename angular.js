f248c630459732f556ed9192189aa99f76106c66
jenkins <ci-bot@angularjs.org>
2017-07-03 05:37:38 -0700

    v1.6.5-build.5417+sha.1991e77

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.5-build.5416+sha.57a972d
+ * @license AngularJS v1.6.5-build.5417+sha.1991e77
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -106,7 +106,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.5-build.5416+sha.57a972d/' +
+    message += '\nhttp://errors.angularjs.org/1.6.5-build.5417+sha.1991e77/' +
       (module ? module + '/' : '') + code;
 
     for (i = 0, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2768,7 +2768,7 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.5-build.5416+sha.57a972d',
+  full: '1.6.5-build.5417+sha.1991e77',
   major: 1,
   minor: 6,
   dot: 5,
@@ -2918,7 +2918,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.6.5-build.5416+sha.57a972d' });
+  .info({ angularVersion: '1.6.5-build.5417+sha.1991e77' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
