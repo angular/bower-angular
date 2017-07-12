a96fb12f370020f5960034dec6bfac42d4ffb6a6
jenkins <ci-bot@angularjs.org>
2017-07-12 03:24:22 -0700

    v1.6.6-build.5425+sha.0616dde

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.6-build.5424+sha.25bf1ef
+ * @license AngularJS v1.6.6-build.5425+sha.0616dde
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -106,7 +106,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.6-build.5424+sha.25bf1ef/' +
+    message += '\nhttp://errors.angularjs.org/1.6.6-build.5425+sha.0616dde/' +
       (module ? module + '/' : '') + code;
 
     for (i = 0, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2768,7 +2768,7 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.6-build.5424+sha.25bf1ef',
+  full: '1.6.6-build.5425+sha.0616dde',
   major: 1,
   minor: 6,
   dot: 6,
@@ -2918,7 +2918,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.6.6-build.5424+sha.25bf1ef' });
+  .info({ angularVersion: '1.6.6-build.5425+sha.0616dde' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
