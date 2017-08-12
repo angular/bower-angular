25d1cd1cef912b60f54ab3e8d3be6fbf4d2a053e
jenkins <ci-bot@angularjs.org>
2017-08-12 02:19:36 -0700

    v1.6.6-build.5451+sha.7c87628

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.6-build.5450+sha.a83d646
+ * @license AngularJS v1.6.6-build.5451+sha.7c87628
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -106,7 +106,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.6-build.5450+sha.a83d646/' +
+    message += '\nhttp://errors.angularjs.org/1.6.6-build.5451+sha.7c87628/' +
       (module ? module + '/' : '') + code;
 
     for (i = 0, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2768,7 +2768,7 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.6-build.5450+sha.a83d646',
+  full: '1.6.6-build.5451+sha.7c87628',
   major: 1,
   minor: 6,
   dot: 6,
@@ -2918,7 +2918,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.6.6-build.5450+sha.a83d646' });
+  .info({ angularVersion: '1.6.6-build.5451+sha.7c87628' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
