9a3609068a7697b399adb29d240b4d67c10e8514
jenkins <ci-bot@angularjs.org>
2017-09-22 13:43:49 -0700

    v1.6.7-build.5469+sha.6d997f5

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.7-build.5468+sha.34237f9
+ * @license AngularJS v1.6.7-build.5469+sha.6d997f5
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -106,7 +106,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.7-build.5468+sha.34237f9/' +
+    message += '\nhttp://errors.angularjs.org/1.6.7-build.5469+sha.6d997f5/' +
       (module ? module + '/' : '') + code;
 
     for (i = 0, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2768,7 +2768,7 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.7-build.5468+sha.34237f9',
+  full: '1.6.7-build.5469+sha.6d997f5',
   major: 1,
   minor: 6,
   dot: 7,
@@ -2918,7 +2918,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.6.7-build.5468+sha.34237f9' });
+  .info({ angularVersion: '1.6.7-build.5469+sha.6d997f5' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
