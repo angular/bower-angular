e77a0d79bf90638dc3ef98061d5b931ee4f42435
jenkins <ci-bot@angularjs.org>
2017-12-19 09:48:13 -0800

    v1.6.8-build.5525+sha.9a521cb

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.8-build.5524+sha.62743a5
+ * @license AngularJS v1.6.8-build.5525+sha.9a521cb
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -106,7 +106,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.8-build.5524+sha.62743a5/' +
+    message += '\nhttp://errors.angularjs.org/1.6.8-build.5525+sha.9a521cb/' +
       (module ? module + '/' : '') + code;
 
     for (i = 0, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -2774,7 +2774,7 @@ function toDebugString(obj, maxDepth) {
 var version = {
   // These placeholder strings will be replaced by grunt's `build` task.
   // They need to be double- or single-quoted.
-  full: '1.6.8-build.5524+sha.62743a5',
+  full: '1.6.8-build.5525+sha.9a521cb',
   major: 1,
   minor: 6,
   dot: 8,
@@ -2924,7 +2924,7 @@ function publishExternalAPI(angular) {
       });
     }
   ])
-  .info({ angularVersion: '1.6.8-build.5524+sha.62743a5' });
+  .info({ angularVersion: '1.6.8-build.5525+sha.9a521cb' });
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
